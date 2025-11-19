4f9842dcc916328e33132cbaf49b300a3113be15
Luffy <luisjaperez0106@gmail.com>
2025-11-19 16:26:16 -0500

    add: bases of the login UI
    Agregadas las bases del UI, falta agregar los controladores, y arreglar
    el bug visual del cursor

--- proyectoFinal/src/main/java/controller/mock/MockLogin.java
+++ proyectoFinal/src/main/java/controller/mock/MockLogin.java
@@ -4,14 +4,20 @@ import controller.LoginInterface;
 import model.User;
 
 public class MockLogin implements LoginInterface {
-    @Override
-    public boolean login(String username, String password) {
-        return false;
+  @Override
+  public boolean login(String username, String password) {
+    if (username == "luffy") {
+      return true;
     }
+    if (password == "01060825") {
+      return true;
+    }
+    return false;
+  }
 
-    @Override
-    public boolean createAccount(User newUser) {
-        return false;
-    }
+  @Override
+  public boolean createAccount(User newUser) {
+    return false;
+  }
 
 }
