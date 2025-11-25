66ea4ef740997922bdfb5d85667a1e154be05ee8
Jose Vergara <cheirblack@gmail.com>
2025-11-25 00:09:31 -0500

    add: herencia, atributos & constructor

--- proyectoFinal/src/main/java/model/Usuarios.java
+++ proyectoFinal/src/main/java/model/Usuarios.java
@@ -1,5 +1,13 @@
 package model;
 
-public class Usuarios {
-    
+public class Usuarios extends Admin {
+    private int tarjeta;
+    private String direccion;
+
+    //constructor
+    public Usuarios(String nombre, boolean isAdmin, String correo, String password, int tarjeta, String direccion) {
+        super(nombre, isAdmin, correo, password);
+        this.tarjeta = tarjeta;
+        this.direccion = direccion;
+    }
 }
