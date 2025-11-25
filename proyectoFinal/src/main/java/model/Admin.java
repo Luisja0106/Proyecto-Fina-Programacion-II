80763938ee8a83a3359c7767301f5ac1d8c49b9d
Jose Vergara <cheirblack@gmail.com>
2025-11-24 23:47:58 -0500

    Add: atributos & constructor

--- proyectoFinal/src/main/java/model/Admin.java
+++ proyectoFinal/src/main/java/model/Admin.java
@@ -1,5 +1,17 @@
 package model;
 
 public class Admin {
+    private String nombre;
+    private boolean isAdmin;
+    private String correo;
+    private String password;
+
+    //metodo constructor
+    public Admin(String nombre, boolean isAdmin, String correo, String password) {
+        this.nombre = nombre;
+        this.isAdmin = isAdmin;
+        this.correo = correo;
+        this.password = password;
+    }
 
 }
