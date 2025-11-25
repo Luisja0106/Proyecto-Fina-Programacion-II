0644c2cea3efbadea95c0615446f32253f3b0840
Luffy <luisjaperez0106@gmail.com>
2025-11-25 15:05:41 -0500

    fix: arregladas las rutas y problemas con los modelos y controladores

--- proyectoFinal/src/main/java/controller/Listas.java
+++ proyectoFinal/src/main/java/controller/Listas.java
@@ -1,61 +1,60 @@
-package controlador;
+package controller;
 
-import modelo.Nodo;
-
+import model.Nodo;
 
 public class Listas<T> {
 
-    public Nodo<T> cabecera;
+  public Nodo<T> cabecera;
 
-    public Listas() {
-        this.cabecera = null;
+  public Listas() {
+    this.cabecera = null;
+  }
+
+  public boolean getEsVacia() {
+    return cabecera == null;
+  }
+
+  // agregar al final
+  public boolean addF(T info) {
+    Nodo<T> nuevo = new Nodo<>(info);
+
+    if (getEsVacia()) {
+      cabecera = nuevo;
+      cabecera.sig = cabecera;
+      cabecera.ant = cabecera;
+    } else {
+      Nodo<T> ultimo = cabecera.ant;
+
+      nuevo.sig = cabecera;
+      nuevo.ant = ultimo;
+
+      ultimo.sig = nuevo;
+      cabecera.ant = nuevo;
     }
+    return true;
+  }
 
-    public boolean getEsVacia() {
-        return cabecera == null;
-    }
+  public Nodo<T> getUltimo() {
+    if (getEsVacia())
+      return null;
+    return cabecera.ant;
+  }
 
-    // agregar al final
-    public boolean addF(T info) {
-        Nodo<T> nuevo = new Nodo<>(info);
+  public void setVaciar() {
+    cabecera = null;
+  }
 
-        if (getEsVacia()) {
-            cabecera = nuevo;
-            cabecera.sig = cabecera;
-            cabecera.ant = cabecera;
-        } else {
-            Nodo<T> ultimo = cabecera.ant;
+  public int getTamano() {
+    if (getEsVacia())
+      return 0;
 
-            nuevo.sig = cabecera;
-            nuevo.ant = ultimo;
+    int cont = 0;
+    Nodo<T> actual = cabecera;
+    do {
+      cont++;
+      actual = actual.sig;
+    } while (actual != cabecera);
 
-            ultimo.sig = nuevo;
-            cabecera.ant = nuevo;
-        }
-        return true;
-    }
-
-    public Nodo<T> getUltimo() {
-        if (getEsVacia()) return null;
-        return cabecera.ant;
-    }
-
-
-    public void setVaciar() {
-        cabecera = null;
-    }
-
-
-    public int getTamano() {
-        if (getEsVacia()) return 0;
-
-        int cont = 0;
-        Nodo<T> actual = cabecera;
-        do {
-            cont++;
-            actual = actual.sig;
-        } while (actual != cabecera);
-
-        return cont;
-    }
+    return cont;
+  }
 }
