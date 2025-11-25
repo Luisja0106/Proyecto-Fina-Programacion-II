3add32df2ae43f27b6adcf90dae50d5211d2192d
Luffy <luisjaperez0106@gmail.com>
2025-11-25 11:29:07 -0500

    Add:CatalogoController y producto.fxml
    Se añade el controlador del catalogo, y la base o blueprint para los
    productos añadidos a la grid

---
+++ proyectoFinal/src/main/java/controller/viewControllers/CatalogoController.java
@@ -0,0 +1,23 @@
+package controller.viewControllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+
+public class CatalogoController {
+
+  @FXML
+  private TextField buscador;
+
+  @FXML
+  private ComboBox<?> cbFiltro;
+
+  @FXML
+  private ScrollPane center;
+
+  @FXML
+  private Label left_square_title;
+
+}
