247f7d80695a30dd39a76d730b2f56c04f5c18d2
Luffy <luisjaperez0106@gmail.com>
2025-11-26 23:01:11 -0500

    feat: añadida funcion de cambio de color
    Se añade la funcion de cambiar de color los iconos en hover, ademas se
    mejoran algunos iconos

--- proyectoFinal/src/main/java/controller/viewControllers/ProductController.java
+++ proyectoFinal/src/main/java/controller/viewControllers/ProductController.java
@@ -1,11 +1,14 @@
 package controller.viewControllers;
 
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import utils.InputDialog;
 
 public class ProductController {
@@ -26,6 +29,13 @@ public class ProductController {
   private Label txtPrecio;
 
   @FXML
+  private HBox cartBtnContainer;
+  @FXML
+  private FontAwesomeIconView favIcon;
+
+  private boolean isInFav = false;
+
+  @FXML
   void addCarrito(ActionEvent event) {
     InputDialog.information("Se ha añadido al carrito", "El producto se ha añadido al carrito satisfactoriamente");
 
@@ -33,7 +43,15 @@ public class ProductController {
 
   @FXML
   void addFavs(ActionEvent event) {
-    InputDialog.information("Se ha añadido a favoritos", "El producto se ha añadido a favoritos satisfactoriamente");
+    if (!isInFav) {
+      favIcon.setGlyphName(FontAwesomeIcon.HEART.name());
+      favIcon.setStyle("-fx-fill:#c00b0b;");
+      isInFav = true;
+    } else {
+      favIcon.setGlyphName(FontAwesomeIcon.HEART_ALT.name());
+      favIcon.setStyle("-fx-fill:#BFC0C2;");
+      isInFav = false;
+    }
   }
 
   public void setProducto(String nom, float precio, String ruta) {
