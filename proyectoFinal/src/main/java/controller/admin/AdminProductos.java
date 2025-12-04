387d0317b22be1d7e960d93b260004b10036edde
Luffy <luisjaperez0106@gmail.com>
2025-12-03 20:52:43 -0500

    feat: añadido de la funcion de cargar los objetos

--- proyectoFinal/src/main/java/controller/admin/AdminProductos.java
+++ proyectoFinal/src/main/java/controller/admin/AdminProductos.java
@@ -3,14 +3,36 @@ package controller.admin;
 import controller.Listas;
 import controller.ProductoLista;
 import model.Admin;
+import model.Nodo;
 import model.Productos;
 
 public class AdminProductos {
   private Admin user;
-  private Listas<Productos> productos;
 
   public boolean isAdmin() {
     return user.isAdmin();
   }
 
+  // metodo para cargar la lista personal del admin
+  public boolean setLista() {
+    if (!isAdmin()) // verificaciones de que sea admin
+      return false;
+    ProductoLista todos = new ProductoLista(true);
+    Nodo<Productos> aux = todos.cabecera;
+    Listas<Productos> productos = new Listas<>();
+    do {
+      String vendedor = aux.info.getNomVendedor();
+      if (vendedor.equals(user.getNombre())) {
+        productos.addF(aux.info);
+      }
+      aux = aux.sig;
+    } while (aux != todos.cabecera);
+    if (!productos.getEsVacia())
+      return false;
+    user.setProductos(productos);
+    return true;
+  }
+
+  private void addLista() {
+  }
 }
